cfcac161fa615d40d781cd83c6c806feb5f268e9
Johan Verrecchia <johanverrecchia@mac.students.clients.local>
2025-05-01 14:09:17 +0200

    new data_load

--- load_data.py
+++ load_data.py
@@ -3,30 +3,25 @@ import os
 
 # Load the dataset
 def load(csv: str) -> tuple[pd.DataFrame, pd.DataFrame]: 
-    
-    # parent folder
+    # Get script directory
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    # absolute path to the CSV file
     file_path = os.path.join(script_dir, csv)
 
     df = pd.read_csv(file_path)
-    
-    raw_feature_cols = [col for col in df.columns if any(prefix in col for prefix in ['HR_', 'EDA_', 'TEMP_'])]
-    meta_cols = ['Team_ID', 'Round', 'Phase', 'Puzzler', 'raw_data_path', 'Individual', 'original_ID', 'Cohort']
-    questionnaire_cols = ['Frustrated', 'upset', 'hostile', 'alert', 'ashamed', 'inspired', 'nervous', 'attentive', 'afraid', 'active', 'determined']
-    
 
-    # split into raw features + questionnaire and metadata
-    raw_data = df[raw_feature_cols + questionnaire_cols]
+    # Define column groups
+    raw_feature_cols = [col for col in df.columns if any(prefix in col for prefix in ['HR_', 'EDA_', 'TEMP_'])]
+    questionnaire_cols = ['Frustrated', 'upset', 'hostile', 'alert', 'ashamed',
+                          'inspired', 'nervous', 'attentive', 'afraid', 'active', 'determined']
+    meta_cols = ['Team_ID', 'Round', 'Puzzler', 'raw_data_path', 'Individual', 'original_ID', 'Cohort']
+
+    # Include 'Phase' with raw data
+    raw_data = df[raw_feature_cols + questionnaire_cols + ['Phase'] + ['Puzzler']]
     metadata = df[meta_cols]
 
     return raw_data, metadata
 
 
 if __name__ == "__main__":
-    raw_data, metadata = load("data/HR_data_2.csv")
-    print("Raw data:")
-    print(raw_data.head())
-    print("\nMetadata:")
-    print(metadata.head())
+    df = load("data/HR_data_2.csv")
+    print(df.head())
