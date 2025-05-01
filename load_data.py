3df6a169e205779df0a06e55652704d07db62f15
Johan Verrecchia <johanverrecchia@mac.students.clients.local>
2025-05-01 13:36:13 +0200

    split data into biosignals and rest

--- load_data.py
+++ load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # Load the dataset
-def load(csv: str) -> pd.DataFrame: 
+def load(csv: str) -> tuple[pd.DataFrame, pd.DataFrame]: 
     
     # parent folder
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,9 +11,22 @@ def load(csv: str) -> pd.DataFrame:
     file_path = os.path.join(script_dir, csv)
 
     df = pd.read_csv(file_path)
-    return df
+    
+    raw_feature_cols = [col for col in df.columns if any(prefix in col for prefix in ['HR_', 'EDA_', 'TEMP_'])]
+    meta_cols = ['Team_ID', 'Round', 'Phase', 'Puzzler', 'raw_data_path', 'Individual', 'original_ID', 'Cohort']
+    questionnaire_cols = ['Frustrated', 'upset', 'hostile', 'alert', 'ashamed', 'inspired', 'nervous', 'attentive', 'afraid', 'active', 'determined']
+    
+
+    # split into raw features + questionnaire and metadata
+    raw_data = df[raw_feature_cols + questionnaire_cols]
+    metadata = df[meta_cols]
+
+    return raw_data, metadata
 
 
 if __name__ == "__main__":
-    df = load("data/HR_data_2.csv")
-    print(df.head())
+    raw_data, metadata = load("data/HR_data_2.csv")
+    print("Raw data:")
+    print(raw_data.head())
+    print("\nMetadata:")
+    print(metadata.head())
